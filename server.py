6d64d1d60e1e5756f31e087c399f436a2474b91e
Krzysztof Kolendowicz <koloses@gmail.com>
2018-12-23 21:37:01 +0100

    Add code air data downloading code to the server Now it returns json with air parameters from WIOŚ weather station in Nowa Sól

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import SocketServer
 import json
 import cgi
+import requests
+import datetime
 
 class Server(BaseHTTPRequestHandler):
     def _set_headers(self):
@@ -14,8 +15,96 @@ class Server(BaseHTTPRequestHandler):
         
     # GET sends back a Hello world message
     def do_GET(self):
+        s = requests.Session()
+        date = datetime.datetime.today().strftime('%d.%m.%Y')
+
+        payload = {'query': '{"measType":"Auto","viewType":"Station","dateRange":"Day","date":"'+date+'","viewTypeEntityId":"193","channels":[624,642,628,646,644,649,645,622,630,626,629,623,650,643]}'}
+        r = s.post('http://80.53.180.198/dane-pomiarowe/pobierz', data=payload)
+        air = r.json()
+
+        nazwa = air['data']['title']
+		#select keys needed
+        for serie in air['data']['series']:
+			
+            if(serie['label'] == 'Benzen'):
+                for data in serie['data']:
+                    benzen = data[1]+serie['unit']
+
+            if(serie['label'] == 'Ci\u015bnienie atmosferyczne'):
+                for data in serie['data']:
+                    cisnienie = data[1]+serie['unit']
+					
+            if(serie['label'] == 'Dwutlenek azotu'):
+                for data in serie['data']:
+                    no2 = data[1]+serie['unit']
+					
+            if(serie['label'] == 'Etylobenzen'):
+                for data in serie['data']:
+                    etylobenzen = data[1]+serie['unit']
+					
+            if(serie['label'] == 'Kierunek wiatru'):
+                for data in serie['data']:
+                    kierwiatr = data[1]+serie['unit']
+					
+            if(serie['label'] == 'M-P-Ksylen'):
+                for data in serie['data']:
+                    mpksylen = data[1]+serie['unit']
+					
+            if(serie['label'] == 'Pr\u0119dko\u015b\u0107 wiatru'):
+                for data in serie['data']:
+                    predkoscwiatru = data[1]+serie['unit']
+					
+            if(serie['label'] == 'Py\u0142 zawieszony PM10'):
+                for data in serie['data']:
+                    pm10 = data[1]+serie['unit']
+					
+            if(serie['label'] == 'Py\u0142 zawieszony PM2.5'):
+                for data in serie['data']:
+                    pm25 = data[1]+serie['unit']
+					
+            if(serie['label'] == 'Temperatura'):
+                for data in serie['data']:
+                    temperatura = data[1]+serie['unit']
+					
+            if(serie['label'] == 'Tlenek azotu'):
+                for data in serie['data']:
+                    no = data[1]+serie['unit']
+					
+            if(serie['label'] == 'Tlenki azotu'):
+                for data in serie['data']:
+                    tlenkiazotu = data[1]+serie['unit']
+					
+            if(serie['label'] == 'Toluen'):
+                for data in serie['data']:
+                    toluen = data[1]+serie['unit']
+					
+            if(serie['label'] == 'Wilgotno\u015b\u0107 wzgl\u0119dna'):
+                for data in serie['data']:
+                    wilgotnoscwzgledna = data[1]+serie['unit']
+				
         self._set_headers()
-        self.wfile.write(json.dumps({'hello': 'world', 'received': 'ok'}))
+        self.wfile.write(json.dumps({'nazwa': nazwa, 'benzen': benzen}).encode())
+        
+    # POST echoes the message adding a JSON field
+    def do_POST(self):
+        ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+        
+        # refuse to receive non-json content
+        if ctype != 'application/json':
+            self.send_response(400)
+            self.end_headers()
+            return
+            
+        # read the message and convert it into a python dictionary
+        length = int(self.headers.getheader('content-length'))
+        message = json.loads(self.rfile.read(length))
+        
+        # add a property to the object, just to mess with data
+        message['received'] = 'ok'
+        
+        # send the message back
+        self._set_headers()
+        self.wfile.write(json.dumps(message))
         
 def run(server_class=HTTPServer, handler_class=Server, port=8008):
     server_address = ('', port)
